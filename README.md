f3d1c32753a5cdd3c4b179a427e10088a5446380
Mike Benshoof <chobbes31@gmail.com>
2013-11-17 06:54:32 -0600

    Initial proof-of-concept

--- README.md
+++ README.md
@@ -2,3 +2,58 @@ liqui-cap-online
 ================
 
 Sample integration between Liquibase, Capistrano, and pt-online-schema-change
+
+Purpose
+-------
+
+This is a proof of concept that was simply meant to show the functionality.  Essentailly, this project handles the following:
+
+* Capturing the output of Liquibase rather than running it directly
+* Parsing each statement to determine the type
+* Converting CREATE INDEX statements to ALTER TABLE statements
+* Switching ALTER statements to use pt-online-schema-change 
+
+The goal is to allow non-blocking deployments of database changes within a larger deployment (i.e. Capistrano) environment.
+
+Requirements
+------------
+
+This system was built on the following test environemnt:
+
+* Centos 6.4 (local VM instance)
+* Percona Server 5.5 (local instance)
+* Capistrano (2.9.0) [website](http://www.capistranorb.com)
+  * Requires Ruby, Rubygem (1.8.7 used for test instance)
+* Liquibase (2.0.5) [website](http://www.liquibase.org)
+  * Requires java (1.5 used for testing) and java MySQL connector
+* pt-online-schema-change [website](http://www.percona.com/doc/percona-toolkit/2.2/pt-online-schema-change.html)
+
+Naturally, setting these up is outside the scope of this README, but one thing I will note - when setting up using standard "yum" commands, you 
+will need to specify the version of Capistrano as the current version doesn't work with the default Ruby/Rubygem install:
+
+```
+yum install ruby
+yum install gcc g++ make automake autoconf curl-devel openssl-devel zlib-devel httpd-devel apr-devel apr-util-devel sqlite-devel
+yum install ruby-rdoc ruby-devel
+yum install rubygems
+gem update --system
+gem install capistrano -v 2.9.0
+```
+
+Samples
+-------
+
+As part of this repo, I've included some sample changelog files that display different ALTER operations.
+
+To use, simply create a new, empty database (mydb in this case) and then run the capfile **run_liquibase_alters** task 3 times, each 
+time changing the real changelog file the master file points to:
+
+```
+<include file="samles/changelog_01.xml" relativeToChangelogFile="true"/>
+```
+
+Once you've applied the 3 changelog files, you can run the **clean_old_tables** task to drop the old tables left by pt-osc. 
+__Note - these tables are left by adding the --no-drop-table flag to pt-osc to help with rollbacks__
+
+The samples directory also contains the mysqldump output of the test schema after applying the chnages and also one showing the cleanup.
+ 
---
+++ samples/mydb.clean.sql
@@ -0,0 +1,117 @@
+-- MySQL dump 10.13  Distrib 5.5.34, for Linux (x86_64)
+--
+-- Host: localhost    Database: mydb
+-- ------------------------------------------------------
+-- Server version	5.5.34-32.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `DATABASECHANGELOG`
+--
+
+DROP TABLE IF EXISTS `DATABASECHANGELOG`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `DATABASECHANGELOG` (
+  `ID` varchar(63) NOT NULL,
+  `AUTHOR` varchar(63) NOT NULL,
+  `FILENAME` varchar(200) NOT NULL,
+  `DATEEXECUTED` datetime NOT NULL,
+  `ORDEREXECUTED` int(11) NOT NULL,
+  `EXECTYPE` varchar(10) NOT NULL,
+  `MD5SUM` varchar(35) DEFAULT NULL,
+  `DESCRIPTION` varchar(255) DEFAULT NULL,
+  `COMMENTS` varchar(255) DEFAULT NULL,
+  `TAG` varchar(255) DEFAULT NULL,
+  `LIQUIBASE` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`ID`,`AUTHOR`,`FILENAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `DATABASECHANGELOGLOCK`
+--
+
+DROP TABLE IF EXISTS `DATABASECHANGELOGLOCK`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `DATABASECHANGELOGLOCK` (
+  `ID` int(11) NOT NULL,
+  `LOCKED` tinyint(1) NOT NULL,
+  `LOCKGRANTED` datetime DEFAULT NULL,
+  `LOCKEDBY` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `department`
+--
+
+DROP TABLE IF EXISTS `department`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `department` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `active` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `idx_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_department`
+--
+
+DROP TABLE IF EXISTS `user_department`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_department` (
+  `user_id` int(11) NOT NULL,
+  `department_id` int(11) NOT NULL,
+  `active` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`user_id`,`department_id`),
+  KEY `idx_depts` (`department_id`),
+  CONSTRAINT `fk_dept_id` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`),
+  CONSTRAINT `_fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `active` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_username` (`username`),
+  KEY `idx_active` (`active`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-11-09  9:29:52
